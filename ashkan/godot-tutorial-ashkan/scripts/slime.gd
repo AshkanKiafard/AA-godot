f64a9ccee6386ff82dd04418799459a4b9a5f83d
AshkanKiafard <ashkan.game.dev@gmail.com>
2024-10-09 23:27:55 +0200

    player can spawn friendly slime to destroy enemy slime + added swimming

--- ashkan/godot-tutorial-ashkan/scripts/slime.gd
+++ ashkan/godot-tutorial-ashkan/scripts/slime.gd
@@ -1,12 +1,19 @@
-extends Node2D
+class_name Slime
+extends CharacterBody2D
 
 const SPEED = 60
-
+const JUMP_VELOCITY = -300.0
 var direction = 1
 
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	move_and_slide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -17,4 +24,3 @@ func _process(delta: float) -> void:
 		direction = 1
 		animated_sprite.flip_h = false
 	position.x += direction * SPEED * delta
-	
--- ashkan/godot-tutorial-ashkan/scripts/water.gd
+++ ashkan/godot-tutorial-ashkan/scripts/water.gd
@@ -3,8 +3,9 @@ extends TileMapLayer
 @onready var player: CharacterBody2D = $"../Player"
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	player.in_water = true
+	if body is Player:
+		player.in_water = true
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	player.in_water = false
-	
+	if body is Player:
+		player.in_water = false
