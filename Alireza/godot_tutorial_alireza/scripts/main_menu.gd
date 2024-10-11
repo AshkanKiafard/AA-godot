1c01ef5b98644f51e57909a035ef6a7598f598e9
Alireza Saei <saeialireza359@gmail.com>
2024-10-11 12:50:33 +0330

    Added a cool feature to the main menu start

--- Alireza/godot_tutorial_alireza/scripts/main_menu.gd
+++ Alireza/godot_tutorial_alireza/scripts/main_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@onready var player: CharacterBody2D = $menu_player
+@onready var timer: Timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,7 +14,9 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	player.velocity.x = -100
+	timer.start(1.5)
+	$VBoxContainer/Start.disabled = true
 
 
 func _on_options_pressed() -> void:
@@ -21,3 +25,7 @@ func _on_options_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
