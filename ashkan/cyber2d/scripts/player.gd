dbb364c50142100bc54b40fda2dfb6d85c61a147
AshkanKiafard <ashkan.game.dev@gmail.com>
2024-10-15 15:13:14 +0200

    implemented multiplayer with another approach

--- ashkan/cyber2d/scripts/player.gd
+++ ashkan/cyber2d/scripts/player.gd
@@ -4,18 +4,17 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var camera: Camera2D = $Camera2D
 @export var bullet: PackedScene
 
 func _ready() -> void:
-	$MultiplayerSynchronizer.set_multiplayer_authority(name.to_int())
+	camera.enabled = is_multiplayer_authority()
 
 func _physics_process(delta: float) -> void:
-	if $MultiplayerSynchronizer.get_multiplayer_authority() != multiplayer.get_unique_id():
+	if !is_multiplayer_authority:
 		return
 	handle_movement(delta)
-	if Input.is_action_pressed("fire"):
-		handle_shooting.rpc()
-	$GunRotation.look_at(get_viewport().get_mouse_position())
+	handle_gun()
 
 func handle_movement(delta: float):
 	# Add the gravity.
@@ -35,9 +34,10 @@ func handle_movement(delta: float):
 
 	move_and_slide()
 
-@rpc("any_peer", "call_local")
-func handle_shooting():
-	var shooted_bullet = bullet.instantiate()
-	shooted_bullet.global_position = $GunRotation/BulletSpawn.global_position
-	shooted_bullet.global_rotation = $GunRotation.global_rotation
-	get_tree().root.add_child(shooted_bullet)
+func handle_gun():
+	$GunRotation.look_at(get_viewport().get_mouse_position())
+	if Input.is_action_pressed("fire"):
+		var shooted_bullet = bullet.instantiate()
+		shooted_bullet.global_position = $GunRotation/BulletSpawn.global_position
+		shooted_bullet.global_rotation = $GunRotation.global_rotation
+		get_tree().root.add_child(shooted_bullet)
