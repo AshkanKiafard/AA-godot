02ee5f62ecc145e2ef45420ea71eac93ea4d4b34
AshkanKiafard <ashkan.game.dev@gmail.com>
2024-10-18 04:43:15 +0200

    made animations sync together when starting attack, added grunt noises when attacking, stamina tweeks

--- ashkan/cyber2d/scripts/player.gd
+++ ashkan/cyber2d/scripts/player.gd
@@ -7,13 +7,16 @@ extends CharacterBody2D
 @onready var legs_sprite: AnimatedSprite2D = $Legs
 @onready var ground_ray_cast: RayCast2D = $GroundRayCast
 @onready var voice: AudioStreamPlayer2D = $voice
+@onready var grunt_timer: Timer = $GruntTimer
 
 const WOO_AUDIO = preload("res://assets/audio/characters/Biker/miscellaneous_4_sean.wav")
 const SHOUUTING_AUDIO = preload("res://assets/audio/characters/Biker/shouting_7_sean.wav")
 const GRUNT_AUDIOS = {
 	"jump": [preload("res://assets/audio/characters/Biker/grunting_1_sean.wav"),
 	preload("res://assets/audio/characters/Biker/grunting_2_sean.wav")],
-	"attack":[preload("res://assets/audio/characters/Biker/grunting_3_sean.wav"),
+	"attack":[preload("res://assets/audio/characters/Biker/grunting_1_sean.wav"),
+	preload("res://assets/audio/characters/Biker/grunting_2_sean.wav"),
+	preload("res://assets/audio/characters/Biker/grunting_3_sean.wav"),
 	preload("res://assets/audio/characters/Biker/grunting_4_sean.wav"),
 	preload("res://assets/audio/characters/Biker/grunting_5_sean.wav"),
 	preload("res://assets/audio/characters/Biker/grunting_6_sean.wav"),
@@ -21,7 +24,7 @@ const GRUNT_AUDIOS = {
 	preload("res://assets/audio/characters/Biker/grunting_8_sean.wav")]
 	}
 
-const SPEED = 300.0
+const SPEED = 350.0
 const JUMP_VELOCITY = -400.0
 var jump_count = 0
 var jump_available = true
@@ -29,9 +32,14 @@ var jump_available = true
 var x_direction
 var x_speed
 
+var last_frame
+var last_anim
+var last_melee
+var last_attack
 var attack
 
 var shout = true
+var grunt = true
 
 signal health_changed
 var health := 100.0 :
@@ -61,7 +69,6 @@ func restore_stamina(value):
 
 func is_dashing():
 	return player_sprite.is_playing() and player_sprite.animation == "dash"
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -96,25 +103,20 @@ func handle_movement():
 
 	# Get the input direction and handle the movement/deceleration.
 	x_direction = Input.get_axis("move_left", "move_right")
-	var flip_h = false if x_direction == 1 else true if x_direction == -1 else player_sprite.flip_h
-	if player_sprite.flip_h != flip_h:
-		player_sprite.flip_h = flip_h
-		weapon_sprite.flip_h = flip_h
-		hand_sprite.flip_h = flip_h
-		legs_sprite.flip_h = flip_h
-		# correct the sprite offset
-		player_sprite.position.x += 40 * x_direction
-		weapon_sprite.position.x += 40 * x_direction
-		legs_sprite.position.x += 40 * x_direction
-		hand_sprite.position.x += 20 * x_direction
-		hand_sprite.rotation += 45 * x_direction
-		
+	
 	var regen_stamina = true
-	attack = Input.is_action_pressed("attack")
+	attack = Input.is_action_pressed("attack") and stamina > 5
 	if attack:
-		# TODO play grunting sound
+		if grunt:
+			play_voice(1+randf_range(-0.1, 0.1), choose(GRUNT_AUDIOS["attack"]))
+			grunt = false
+			grunt_timer.wait_time = randf_range(2, 5)
+			grunt_timer.start()
 		stamina -= 0.1 if armed else 0.05
 		regen_stamina = false
+	else:
+		grunt = true
+		grunt_timer.stop()
 		
 	x_speed = SPEED
 	if x_direction:
@@ -126,18 +128,18 @@ func handle_movement():
 			play_voice(1+randf_range(-0.05, 0.05), WOO_AUDIO)
 			play_anim("dash", false, false)
 			regen_stamina = false
-			stamina -= 50
+			stamina -= 75
 		if is_dashing():
-			x_speed *= 7
+			x_speed *= 10
 		velocity.x = x_direction * x_speed
 		if regen_stamina:
-			restore_stamina(1)
+			restore_stamina(0.3)
 	else:
 		if regen_stamina:
-			restore_stamina(1.5)
+			restore_stamina(1)
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()	
+	move_and_slide()
 
 func handle_animation():
 	# if hurt_timer.is_stopped():
@@ -154,11 +156,25 @@ func handle_animation():
 		if jump_count == 0 and player_sprite.animation != "jump":
 			play_anim("jump", false, false)
 
-func play_anim(anim:String, attack: bool, melee: bool):
+func play_anim(anim:String, is_attacking: bool, melee: bool):
 	# reset sprites
 	player_sprite.position.y = -48
 	hand_sprite.visible = false
 	legs_sprite.visible = false
+	
+	# flip sprites
+	var flip_h = false if x_direction == 1 else true if x_direction == -1 else player_sprite.flip_h
+	if player_sprite.flip_h != flip_h:
+		player_sprite.flip_h = flip_h
+		weapon_sprite.flip_h = flip_h
+		hand_sprite.flip_h = flip_h
+		legs_sprite.flip_h = flip_h
+		# correct the sprite offset
+		player_sprite.position.x += 40 * x_direction
+		weapon_sprite.position.x += 40 * x_direction
+		legs_sprite.position.x += 40 * x_direction
+		hand_sprite.position.x += 20 * x_direction
+		hand_sprite.rotation += 45 * x_direction
 	
 	# can't attack when jump-/dashing
 	if "jump" in anim or "dash" in anim:
@@ -168,31 +184,39 @@ func play_anim(anim:String, attack: bool, melee: bool):
 		weapon_sprite.visible = armed
 	
 	var melee_str = "_melee" if melee else ""
-	var attack_str = "_attack" if attack else ""
+	var attack_str = "_attack" if is_attacking else ""
 	
 	player_sprite.play(anim+attack_str+melee_str)
 	
-	# walk does not have an attack animation
-	if anim != "walk":
-		weapon_sprite.play(anim+attack_str)
-	elif anim == "walk" and melee:
-		if not attack:
-			# add extra hand for holding melee weapon while walking
-			weapon_sprite.animation = "idle"
-			weapon_sprite.frame = 0
-			hand_sprite.visible = true
-		else:
-			# stich upper and lower body together
-			# upper: idle attack
-			# lower: walk
-			legs_sprite.visible = true
-			weapon_sprite.play("idle_attack")
-			legs_sprite.play("walk")
-			player_sprite.position.y -= 10
+	# walk does not have an melee attack animation
+	if melee:
+		if anim != "walk":
+			weapon_sprite.play(anim+attack_str)
+		elif anim == "walk":
+			if not is_attacking:
+				# add extra hand for holding melee weapon while walking
+				weapon_sprite.animation = "idle"
+				weapon_sprite.frame = 0
+				hand_sprite.visible = true
+			else:
+				# stich upper and lower body together
+				# upper: idle attack
+				# lower: walk
+				legs_sprite.visible = true
+				weapon_sprite.play("idle_attack")
+				legs_sprite.play("walk")
+				player_sprite.position.y -= 10
 	
 	# sync
+	if anim == last_anim and (last_melee != melee or last_attack != attack):#
+		player_sprite.frame = last_frame + 1
 	weapon_sprite.frame = player_sprite.frame
 	legs_sprite.frame = player_sprite.frame
+	
+	last_anim = anim
+	last_melee = melee
+	last_attack = is_attacking
+	last_frame = player_sprite.frame
 
 func play_voice(pitch_scale, audio):
 	voice.pitch_scale = pitch_scale
@@ -201,3 +225,6 @@ func play_voice(pitch_scale, audio):
 
 func choose(l: Array):
 	return l[randi()%len(l)]
+
+func _on_grunt_timer_timeout() -> void:
+	grunt = true
