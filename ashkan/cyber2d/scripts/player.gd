61f8d73183556f82a9b594f725e9ffd3cec7a467
AshkanKiafard <ashkan.game.dev@gmail.com>
2024-10-18 02:55:49 +0200

    added melee weapon with animations (duct taped walk animations)

--- ashkan/cyber2d/scripts/player.gd
+++ ashkan/cyber2d/scripts/player.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
-@onready var animated_sprite: AnimatedSprite2D = $BikerSprite
+@export var armed: bool
+@onready var player_sprite: AnimatedSprite2D = $PlayerSprite
+@onready var weapon_sprite: AnimatedSprite2D = $MeleeWeapon
+@onready var hand_sprite: Sprite2D = $Hand
+@onready var legs_sprite: AnimatedSprite2D = $Legs
 @onready var ground_ray_cast: RayCast2D = $GroundRayCast
 @onready var voice: AudioStreamPlayer2D = $voice
 
@@ -25,6 +29,8 @@ var jump_available = true
 var x_direction
 var x_speed
 
+var attack
+
 var shout = true
 
 signal health_changed
@@ -39,7 +45,7 @@ var health := 100.0 :
 				pass
 				#hurt_audio.play()
 				#hurt_timer.start()
-				#animated_sprite.play("hurt")
+				#player_sprite.play("hurt")
 			health = clamp(value, 0, 100)
 			health_changed.emit()
 
@@ -54,12 +60,13 @@ func restore_stamina(value):
 		stamina += value
 
 func is_dashing():
-	return animated_sprite.is_playing() and animated_sprite.animation == "dash"
+	return player_sprite.is_playing() and player_sprite.animation == "dash"
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
+		weapon_sprite.visible = false
 		velocity += get_gravity() * delta
 		if not ground_ray_cast.is_colliding() and not voice.playing and shout:
 			shout = false
@@ -67,36 +74,49 @@ func _physics_process(delta: float) -> void:
 	else:
 		jump_count = 0
 		shout = true
-	handle_movement(delta)
+	handle_movement()
 	handle_animation()
 
-func handle_movement(delta: float):
-	# Handle jump.
+func handle_movement():
+	# Handle jump
+	# TODO add coyote time
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			jump_count = 0
 		jump_count += 1
 		if jump_count <= 3:
 			if jump_count == 1:
-				animated_sprite.play("jump")
+				play_anim("jump", false, false)
 			else:
-				animated_sprite.play("double_jump")
+				play_anim("double_jump", false, false)
 			if randi() % 2:
 				play_voice(1, choose(GRUNT_AUDIOS["jump"]))
 			stamina -= 25
 			velocity.y = JUMP_VELOCITY - (stamina * 2)
 
 	# Get the input direction and handle the movement/deceleration.
-	# TODO add coyote time
 	x_direction = Input.get_axis("move_left", "move_right")
-	var flip_h = false if x_direction == 1 else true if x_direction == -1 else animated_sprite.flip_h
-	if animated_sprite.flip_h != flip_h:
-		animated_sprite.flip_h = flip_h
+	var flip_h = false if x_direction == 1 else true if x_direction == -1 else player_sprite.flip_h
+	if player_sprite.flip_h != flip_h:
+		player_sprite.flip_h = flip_h
+		weapon_sprite.flip_h = flip_h
+		hand_sprite.flip_h = flip_h
+		legs_sprite.flip_h = flip_h
 		# correct the sprite offset
-		animated_sprite.position.x -= 40 * (-x_direction)
+		player_sprite.position.x += 40 * x_direction
+		weapon_sprite.position.x += 40 * x_direction
+		legs_sprite.position.x += 40 * x_direction
+		hand_sprite.position.x += 20 * x_direction
+		hand_sprite.rotation += 45 * x_direction
 		
-	var x_speed = SPEED
 	var regen_stamina = true
+	attack = Input.is_action_pressed("attack")
+	if attack:
+		# TODO play grunting sound
+		stamina -= 0.1 if armed else 0.05
+		regen_stamina = false
+		
+	x_speed = SPEED
 	if x_direction:
 		if Input.is_action_pressed("run"):
 			regen_stamina = false
@@ -104,7 +124,7 @@ func handle_movement(delta: float):
 			stamina -= 0.2
 		if Input.is_action_just_pressed("dash") and stamina > 50:
 			play_voice(1+randf_range(-0.05, 0.05), WOO_AUDIO)
-			animated_sprite.play("dash")
+			play_anim("dash", false, false)
 			regen_stamina = false
 			stamina -= 50
 		if is_dashing():
@@ -113,24 +133,66 @@ func handle_movement(delta: float):
 		if regen_stamina:
 			restore_stamina(1)
 	else:
-		restore_stamina(2)
+		if regen_stamina:
+			restore_stamina(1.5)
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	move_and_slide()
+
+	move_and_slide()	
 
 func handle_animation():
 	# if hurt_timer.is_stopped():
 	if is_on_floor():
 		if not is_dashing():
 			if x_direction == 0:
-				animated_sprite.play("idle1")
+				play_anim("idle", attack, armed)
 			else:
 				if Input.is_action_pressed("run"):
-					animated_sprite.play("run")
+					play_anim("run", attack, armed)
 				else:
-					animated_sprite.play("walk")
+					play_anim("walk", attack, armed)
 	else:
-		if jump_count == 0 and animated_sprite.animation != "jump":
-			animated_sprite.play("jump")
+		if jump_count == 0 and player_sprite.animation != "jump":
+			play_anim("jump", false, false)
+
+func play_anim(anim:String, attack: bool, melee: bool):
+	# reset sprites
+	player_sprite.position.y = -48
+	hand_sprite.visible = false
+	legs_sprite.visible = false
+	
+	# can't attack when jump-/dashing
+	if "jump" in anim or "dash" in anim:
+		weapon_sprite.visible = false
+		melee = false
+	else:
+		weapon_sprite.visible = armed
+	
+	var melee_str = "_melee" if melee else ""
+	var attack_str = "_attack" if attack else ""
+	
+	player_sprite.play(anim+attack_str+melee_str)
+	
+	# walk does not have an attack animation
+	if anim != "walk":
+		weapon_sprite.play(anim+attack_str)
+	elif anim == "walk" and melee:
+		if not attack:
+			# add extra hand for holding melee weapon while walking
+			weapon_sprite.animation = "idle"
+			weapon_sprite.frame = 0
+			hand_sprite.visible = true
+		else:
+			# stich upper and lower body together
+			# upper: idle attack
+			# lower: walk
+			legs_sprite.visible = true
+			weapon_sprite.play("idle_attack")
+			legs_sprite.play("walk")
+			player_sprite.position.y -= 10
+	
+	# sync
+	weapon_sprite.frame = player_sprite.frame
+	legs_sprite.frame = player_sprite.frame
 
 func play_voice(pitch_scale, audio):
 	voice.pitch_scale = pitch_scale
